6d21d0916a19cccd685207c73f1d561f8105816e
himanshu <himanshu@codercrafter.in>
2025-08-05 08:11:04 +0530

    asd

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -171,7 +171,7 @@ export default function TechnicalSupport() {
           </div>
         </div>
       </div>
-
+{/* change */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
         {/* Support Channels */}
         <div className="mb-16">
